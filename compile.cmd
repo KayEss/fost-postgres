7ad465be886d049b38f7f6d506d9c38a06b2fa46
Kirit Sælensminde <k@kirit.com>
2009-06-15 06:25:20 +0000

    Compile now ensures that the database driver can be loaded and a connection made (this doesn't work due to incorrect credentials).

--- compile.cmd
+++ compile.cmd
@@ -1,8 +1,12 @@
-..\bjam toolset=msvc %*
 @echo off
-call ..\boost-version.cmd
+..\bjam toolset=msvc %*
 IF NOT ERRORLEVEL 1 (
+    call ..\boost-version.cmd
     for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
+)
+
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-schema-test-dyndriver pqxx fost-postgres.dll -d "user=Test password=tester host=localhost"
 )
 
 IF NOT ERRORLEVEL 1 (
