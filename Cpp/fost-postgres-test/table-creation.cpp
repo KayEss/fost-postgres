11f186a9d728c84fa93f0abecd254c42a19568ff
Kirit Sælensminde <k@kirit.com>
2009-09-01 08:04:28 +0000

    The Postgres tests now execute and pass and have started to put in where the tests to really check that tables are properly created will go.

---
+++ Cpp/fost-postgres-test/table-creation.cpp
@@ -0,0 +1,18 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-postgres-test.hpp"
+#include <fost/db>
+#include <fost/test>
+
+
+using namespace fostlib;
+
+
+FSL_TEST_SUITE( table_creation );
+
