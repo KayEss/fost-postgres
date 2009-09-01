efc033367b34be521786a9ad8cef9bb432cae95d
Kirit Sælensminde <k@kirit.com>
2009-09-01 08:48:03 +0000

    Now builds tables that contain some of the basic types with both null and non-null columns.

--- Cpp/fost-postgres-test/table-creation.cpp
+++ Cpp/fost-postgres-test/table-creation.cpp
@@ -16,3 +16,28 @@ using namespace fostlib;
 
 FSL_TEST_SUITE( table_creation );
 
+
+namespace {
+    void check_create_table( const string &name, bool not_null ) {
+        dbconnection dbc( read_dsn.value() + L" dbname=FSL_Test", write_dsn.value() + L" dbname=FSL_Test" );
+
+        fostlib::meta_instance types( name );
+        types
+            .primary_key( L"id", L"integer" )
+            .field( L"test_integer", L"integer", not_null )
+            .field( L"test_varchar", L"varchar", not_null, 128 )
+            .field( L"test_text", L"text", not_null )
+            .field( L"test_boolean", L"boolean", not_null )
+        ;
+        dbtransaction transaction( dbc );
+        transaction.create_table( types );
+        transaction.commit();
+    }
+}
+
+
+FSL_TEST_FUNCTION( types ) {
+    check_create_table( "types_not_null", true );
+    check_create_table( "types_null", false );
+}
+
