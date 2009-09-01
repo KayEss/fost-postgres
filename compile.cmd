eaba714139745cd7d7e9b80806f90c21477eda14
Kirit Sælensminde <k@kirit.com>
2009-09-01 09:31:55 +0000

    The postgres tests now pass on Windows.

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,3 @@
 @echo off
 echo fost-postgres
 ..\bjam toolset=msvc %*
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-dyndriver -b false pqxx fost-postgres.dll -d "user=Test password=tester host=localhost dbname=postgres"
-)
