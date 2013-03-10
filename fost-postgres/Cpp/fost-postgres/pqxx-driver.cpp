132281b1e6924286ef42498ec181df4bc7f41de5
Kirit Sælensminde <k@kirit.com>
2013-03-10 08:07:46 +0000

    `fostlib::absorbException` has been deprecated in favour of `fostlib::absorb_exception`.

--- fost-postgres/Cpp/fost-postgres/pqxx-driver.cpp
+++ fost-postgres/Cpp/fost-postgres/pqxx-driver.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2011, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2008-2013,Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -59,7 +59,7 @@ namespace {
         try {
             m_transaction.reset();
         } catch ( ... ) {
-            fostlib::absorbException();
+            fostlib::absorb_exception();
         }
 
         void transaction() {
