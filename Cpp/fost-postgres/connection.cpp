e267fc2cd47f5e530416118f8f4f74f1f0e2eb9c
Kirit Sælensminde <k@kirit.com>
2017-11-08 16:36:43 +0700

    Put session variable names in double quotes.

--- Cpp/fost-postgres/connection.cpp
+++ Cpp/fost-postgres/connection.cpp
@@ -174,7 +174,7 @@ fostlib::pg::connection &fostlib::pg::connection::zoneinfo(const string &zi) {
 
 
 fostlib::pg::connection &fostlib::pg::connection::set_session(const string &n, const string &v) {
-    exec("SET " + n.std_str() + " = " + pimpl->trans->quote(v.std_str()));
+    exec("SET \"" + n.std_str() + "\" = " + pimpl->trans->quote(v.std_str()));
     return *this;
 }
 
