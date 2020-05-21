bdc10d517f366f2c912cbb97460bf9e17574fc3b
Kirit Sælensminde <kirit@kirit.com>
2020-05-21 17:44:03 +0700

    Fix uses of deprecated functions in stress tests.

--- Cpp/fost-postgres-test/config.cpp
+++ Cpp/fost-postgres-test/config.cpp
@@ -1,5 +1,5 @@
 /**
-    Copyright 2008-2019 Red Anchor Trading Co. Ltd.
+    Copyright 2008-2020 Red Anchor Trading Co. Ltd.
 
     Distributed under the Boost Software License, Version 1.0.
     See <http://www.boost.org/LICENSE_1_0.txt>
@@ -13,14 +13,14 @@ using namespace fostlib;
 
 
 const setting<string> read_dsn(
-        L"fost-postgres-test/basic.cpp",
-        L"Postgres tests",
-        L"Read connection",
-        L"pqxx/",
+        "fost-postgres-test/basic.cpp",
+        "Postgres tests",
+        "Read connection",
+        "pqxx/",
         true);
 const setting<string> write_dsn(
-        L"fost-postgres-test/basic.cpp",
-        L"Postgres tests",
-        L"Write connection",
-        L"pqxx/",
+        "fost-postgres-test/basic.cpp",
+        "Postgres tests",
+        "Write connection",
+        "pqxx/",
         true);
